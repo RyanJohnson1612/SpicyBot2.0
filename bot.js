5bf688bea92fcf89385cbd167dab42286758d11e
Ryan <ryanjohnson@rogers.com>
2020-09-22 14:47:35 -0400

    Refactor bot.js and add queue system

--- bot.js
+++ bot.js
@@ -1,92 +1,183 @@
 ﻿const Discord = require('discord.js');
 const ytdl = require('ytdl-core-discord');
 const client = new Discord.Client();
+const prefix = process.env.prefix;
 
 var userCooldowns = {};
-var queue = [];
+var songQueue = null;
 
-client.on('ready',() => {
+//Display status on ready and configures userCooldowns
+client.on('ready', () => {
     console.log('Online');
     setInterval(function(){ for (userx in userCooldowns){if(userCooldowns[userx] > 0){userCooldowns[userx]-= 1;} }; }, 3000);
 })
 
+//
 client.on('message', message => {
-  if(message.content === '!test'){
-    message.channel.send('✓');
+  if(message.content.startsWith(`${prefix}test`)){
+    sendMessage(message, '✓');
   }
 
-  if(message.content === '!quack'){
-    message.delete(3000);
-    if(message.member.voiceChannel && (userCooldowns[message.member.author] == undefined || userCooldowns[message.member.author] == 0)){
-      
-      userCooldowns[message.member.author] = 10;
-      let channel = message.member.voiceChannel;
-      console.log(userCooldowns[message.member.author]);
-      channel.join()
-      .then(connection => {
-        const dispatcher = connection.playFile('./sounds/mac-quack.mp3', {});
-        message.channel.send(':duck:').then(d_msg => { d_msg.delete(3000); });
-        dispatcher.on('end', () =>  {
-          message.member.voiceChannel.leave();
-        })
-      })
-      .catch(console.log);
-    }
-    else{
-      if (message.member.voiceChannel && (userCooldowns[message.member.username] != undefined || userCooldowns[message.member.username] > 0)){
-          message.reply('Cooldown remaining: ' + userCooldowns[message.member.author]).then(d_msg => { d_msg.delete(3000); });;
-      }
-      else{
-          message.reply('You need to join a channel first!').then(d_msg => { d_msg.delete(3000); });
-      }
-    }
+  if(message.content.startsWith(`${prefix}quack`)){
+    playSound(message, './sounds/mac-quack.mp3', ':duck:')
   }
 
-  if(message.content === '!badquack'){
-    message.delete(3000);
-    if(message.member.voiceChannel && (userCooldowns[message.member.author] == undefined || userCooldowns[message.member.author] == 0)){
-      userCooldowns[message.member.author] = 10;
-      message.member.voiceChannel.join()
-      .then(connection => {
-        const dispatcher = connection.playFile('./sounds/birding.mp3', {});
-        message.reply('Use !quack instead, idiot').then(d_msg => { d_msg.delete(5000); });
-        dispatcher.on('end', () =>  {
-          message.member.voiceChannel.leave()
-        })
-      })
-      .catch(console.log);
-    }
-    else{
-      if (message.member.voiceChannel && (userCooldowns[message.member.username] != undefined || userCooldowns[message.member.username] > 0)){
-          message.reply('Cooldown remaining: ' + userCooldowns[message.member.author]).then(d_msg => { d_msg.delete(3000); });;
-      }
-      else{
-          message.reply('You need to join a channel first!').then(d_msg => { d_msg.delete(3000); });;
-      }
-    }
+  if(message.content.startsWith(`${prefix}quackquack`)){
+    playSound(message, './sounds/birding.mp3', ':duck: :duck:')
   }
 
-  if(message.content.includes('!jukebox')){
-    var url = message.content.split('!jukebox')[1];
-
-    message.member.voiceChannel.join()
-      .then(connection => {
-        let videoInfo = await ytdl.getInfo(url);
-        let video = {
-          title: videoInfo.title,
-          url: videoInfo.video_url, 
-        };
-        message.channel.send(`Now playing: ${video.title}`);
-        play(connection, url);
-      })
-      .catch(error => {
-        console.log(error)
-      })
+  if(message.content.startsWith(`${prefix}jukebox`)){
+    startJukebox(message); 
   }
 });
 
-async function play(connection, url) {
-  connection.playOpusStream(await ytdl(url));
+/*
+============================================
+  Functions
+============================================
+*/
+
+/*
+* Send a message in chat current chat channel
+*
+* message: Object
+* botResponse: String
+*
+*/
+
+async function sendMessage(message, botResponse) {
+  message.channel.send(botResponse);
+}
+
+/*
+* Send a message in chat current chat channel then delete message after 
+*
+* message: Object
+* botResponse: String
+* delay: Integer
+*
+*/
+
+async function sendAndDeleteMessage(message, botResponse, delay) {
+  message.channel.send(botResponse).then(msg => { msg.delete(delay); });
+}
+
+/*
+* Play sound file and optionally send a message in chat current chat channel
+*
+* message: Object
+* sound: String
+* botResponse: String
+*
+*/
+async function playSound(message, sound, botResponse) {
+  if(message.member.voiceChannel && (userCooldowns[message.member.author] == undefined || userCooldowns[message.member.author] == 0)){
+    userCooldowns[message.member.author] = 10;
+    let channel = message.member.voiceChannel;
+    channel.join()
+    .then(connection => {
+      const dispatcher = connection.playFile(sound, {});
+
+      if(botResponse) {
+        message.channel.send(botResponse);
+      }
+      dispatcher.on('end', () =>  {
+        message.member.voiceChannel.leave();
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+  else{
+    if (message.member.voiceChannel && (userCooldowns[message.member.username] != undefined || userCooldowns[message.member.username] > 0)){
+        message.reply('Cooldown remaining: ' + userCooldowns[message.member.author]);
+    }
+    else{
+        message.reply('You need to join a channel first!');
+    }
+  }
+}
+
+/*
+* Create song queue if one doesn't exist then join voice channel and add song to queue 
+*
+* message: Object
+*
+*/
+async function startJukebox(message) {
+  let url = message.content.split(' ')[1];
+  let textChannel = message.channel;
+  let voiceChannel = message.member.voiceChannel;
+  let songInfo = await ytdl.getInfo(url);
+
+  let song = {
+    title: songInfo.title,
+    url: songInfo.video_url
+  };
+
+  if(!queue) {
+    const songQueue = {
+      textChannel: textChannel,
+      voiceChannel: voiceChannel,
+      connection: null,
+      songs: [],
+      volume: 5,
+      playing: true
+    }
+
+    songQueue.songs.push(song);
+
+    voiceChannel.join()
+      .then(connection => {
+        songQueue.connection = connection;
+        play(song, songQueue);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  else {
+    songQueue.songs.push(song);
+    sendMessage(`${song.title} added to the queue`);
+  }
+
+  message.member.voiceChannel.join()
+      .then(connection => {
+        play(connection, url);
+      })
+      .catch(error => {
+        sendMessage(message, 'An error occured trying to play this song');
+        console.log(error);
+      })
+}
+/*
+* Play first song in queue, then removes song from queue and play next song. Leaves voice channel if no songs in queue
+*
+* songQueue: Object
+*
+*/
+
+async function play(songQueue) {
+  if(songQueue.songs.length === 0) {
+    songQueue.voiceChannel.leave();
+    songQueue = null;
+    return;
+  }
+  let song = songQueue.songs[0];
+  
+  const dispatcher = songQueue.connection
+    .playOpusStream(await ytdl())
+    .on('finish', () => {
+      songQueue.songs.shift();
+      play(songQueue);
+    })
+    .on('error', error => {
+      console.log('error')
+    })
+    dispatcher.setVolumeLogarithmic(songQueue.volume / 5);
+    songQueue.textChannel.send(`Now playing: ${song.title}`);
 }
 
 client.login(process.env.token);
